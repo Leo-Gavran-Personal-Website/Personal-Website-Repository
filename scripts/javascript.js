674006c68234564c8e78f47bb14f4168612207a2
benjmatthies <benjmatthies@zohomail.com>
2023-04-08 21:44:47 -0600

    adding stuff

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,50 @@
 alert(`You have reached ${document.title}`)
+
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
+
+/*const headerElement = document.querySelector("header");
+const footerElement = document.querySelector("footer");
+
+const fetchHeader = async () => {
+    try {
+        const res = await fetch("header.html");
+        const template = await res.text();
+
+        headerElement.innerHTML = template;
+    } catch (err) {
+        console.log(err);
+    }
+};/**/
+
+/*const fetchFooter = async () => {
+  try {
+    const res = await fetch("./footer.txt");
+    const template = await res.text();
+
+    footerElement.innerHTML = template;
+  } catch (err) {
+    console.log(err);
+  }
+};*/
+
+//fetchHeader();
+//fetchFooter();
